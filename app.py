ec31f9dad1152b90e7e10389549d400ca249501b
pelegsegal2013-design <pelegsegal2013@gmail.com>
2026-04-07 13:39:20 +0300

    Update title in app.py

--- app.py
+++ app.py
@@ -1,16 +1,1 @@
-import streamlit as st
-
-st.title("האתר של פלג 🚀")
-
-# יצרנו תיבה שבה המשתמש כותב את הציון שלו
-grade = st.text_input("מה הציון שלך במתמטיקה?")
-
-# הוספנו כפתור כדי שהבדיקה לא תרוץ "ישר"
-if st.button("בדוק ציון"):
-    if grade: # בודק שלא השארת ריק
-        if int(grade) > 90:
-            st.success("Excellent logic! 🏆")
-            st.disco()
-        else:
-            st.warning("Keep practicing! 💪")
-    st.title("תרשום את הציון שלך לא משהו אחר!")
+st.title("d")
